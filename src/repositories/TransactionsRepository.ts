d9b751d0f46b4ab8a862c6b53747b03127b23aa0
Josué Feitosa <josue.feitosa@outlook.com.br>
2020-05-08 17:14:09 -0300

    Initial commit

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,43 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface Reducer {
+  prevVal: number,
+  transaction: Transaction
+}
+
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const income = transactions.reduce((accumulator, transaction) => {
+      return transaction.type === 'income'
+        ? accumulator + Number(transaction.value)
+        : accumulator;
+    }, 0);
+
+    const outcome = transactions.reduce((accumulator, transaction) => {
+      return transaction.type === 'outcome'
+        ? accumulator + Number(transaction.value)
+        : accumulator;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
+  }
+}
+
+export default TransactionsRepository;
